812b9a072ab057f9ae145a255c3c67ccc2590b3e
Simon <simonjonsson3@hotmail.com>
2024-03-13 16:43:09 +0100

    Add symlink script and fix tmux delay

---
+++ symlinks.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+
+ln -s ~/Repos/dot-configs/nvim ~/.config/nvim
+ln -s ~/Repos/dot-configs/alacritty ~/.config/alacritty
+ln -s ~/Repos/dot-configs/tmu ~/.config/tmux
+ln -s ~/Repos/dot-configs/pypoetry ~/.config/pypoetry
