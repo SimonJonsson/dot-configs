3f4ee76d261d52e3cae6e04fd09458a6295da1c9
Simon <5984660+SimonJonsson@users.noreply.github.com>
2024-11-28 08:36:31 +0100

    Remove Ruff lint

--- nvim/lua/plugins/lang/python-lsp.lua
+++ nvim/lua/plugins/lang/python-lsp.lua
@@ -3,6 +3,15 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
+        ruff = {
+          init_options = {
+            settings = {
+              lint = {
+                enable = false,
+              },
+            },
+          },
+        },
         ruff_lsp = {
           handlers = {
             ["textDocument/publishDiagnostics"] = function() end,
