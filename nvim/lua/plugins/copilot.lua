edc0a4b2e32a810da0dfa82ea6e7720ca281bbc7
Simon <5984660+SimonJonsson@users.noreply.github.com>
2024-12-27 09:33:31 +0100

    Update config to conform to new lazyvim update

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -9,15 +9,6 @@ return {
       return {}
     end,
   },
-  -- Setup nvim-cmp
-  {
-    "nvim-cmp",
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      -- Disable ghost text for nvim-cmp, use copilot suggestion instead
-      opts.experimental.ghost_text = false
-    end,
-  },
   -- Setup copilot.vim
   {
     "github/copilot.vim",
@@ -67,26 +58,6 @@ return {
       end, { nargs = 0 })
 
       keymap("n", "<leader>ac", ":CopilotToggle<CR>", { desc = "Toggle copilot", noremap = true, silent = true })
-    end,
-  },
-  -- Add status line icon for copilot
-  {
-    "nvim-lualine/lualine.nvim",
-    opts = function(_, opts)
-      local Util = require("lazyvim.util")
-      table.insert(opts.sections.lualine_x, 2, {
-        function()
-          local icon = require("lazyvim.config").icons.kinds.Copilot
-          return icon
-        end,
-        cond = function()
-          local ok, clients = pcall(vim.lsp.get_active_clients, { name = "copilot", bufnr = 0 })
-          return ok and #clients > 0
-        end,
-        color = function()
-          return Util.ui.fg("Special")
-        end,
-      })
     end,
   },
 }
