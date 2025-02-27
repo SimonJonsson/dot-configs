d4ea5834138ffd0e6eecb6b16f9845f3b448ff84
Simon <5984660+SimonJonsson@users.noreply.github.com>
2025-02-27 10:03:06 +0100

    Update packages

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -20,6 +20,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.yaml" },
     -- { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "plugins" },
     { import = "plugins.lang" },
   },
