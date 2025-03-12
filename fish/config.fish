945b0259e9cd5ee8c2d41c0784f2b3e5684f2d28
Simon <5984660+SimonJonsson@users.noreply.github.com>
2025-03-12 16:10:49 +0100

    add fish stuff

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# Created by `pipx` on 2025-03-12 09:34:35
+set PATH $PATH /Users/simonjonsson/.local/bin
--- symlinks.sh
+++ symlinks.sh
@@ -5,5 +5,5 @@ ln -s ~/Repos/dot-configs/alacritty ~/.config/alacritty
 ln -s ~/Repos/dot-configs/tmux ~/.config/tmux
 ln -s ~/Repos/dot-configs/pypoetry ~/.config/pypoetry
 ln -s ~/Repos/dot-configs/yazi ~/.config/yazi
-ln -s ~/Repos/dot-configs/aerospace/ ~/.config/aerospace
-ln -s ~/Repos/dot-configs/fish/ ~/.config/fish
+ln -s ~/Repos/dot-configs/aerospace ~/.config/aerospace
+ln -s ~/Repos/dot-configs/fish ~/.config/fish
