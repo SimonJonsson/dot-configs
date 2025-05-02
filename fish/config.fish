e943282fb24b05ee43a3d5dffa719229f7882029
simon <5984660+SimonJonsson@users.noreply.github.com>
2025-05-02 08:36:50 +0200

    Remove fish_variables, ignore it in the future

--- fish/config.fish
+++ fish/config.fish
@@ -10,6 +10,9 @@ alias vim='nvim'
 alias cls='clear'
 alias tmuxkill='tmux kill-server && tmux || tmux'
 alias lg='lazygit'
+alias dbx_stg='rm -f ~/.databrickscfg && cp ~/.databrickscfg_stg ~/.databrickscfg'
+alias dbx_dev='rm -f ~/.databrickscfg && cp ~/.databrickscfg_dev ~/.databrickscfg'
+alias dbx_prd='rm -f ~/.databrickscfg && cp ~/.databrickscfg_prd ~/.databrickscfg'
 
 # Created by `pipx` on 2025-03-14 07:53:37
 set PATH $PATH /Users/simon/.local/bin
