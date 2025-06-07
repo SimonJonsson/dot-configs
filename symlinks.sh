ac63e4951e773fa8c96677aa4216b1d4c03c2f96
simon <5984660+SimonJonsson@users.noreply.github.com>
2025-06-07 14:22:21 +0200

    add hyprland and waybar

--- symlinks.sh
+++ symlinks.sh
@@ -5,5 +5,4 @@ ln -s ~/Repos/dot-configs/alacritty ~/.config/alacritty
 ln -s ~/Repos/dot-configs/tmux ~/.config/tmux
 ln -s ~/Repos/dot-configs/pypoetry ~/.config/pypoetry
 ln -s ~/Repos/dot-configs/yazi ~/.config/yazi
-ln -s ~/Repos/dot-configs/aerospace ~/.config/aerospace
 ln -s ~/Repos/dot-configs/fish ~/.config/fish
